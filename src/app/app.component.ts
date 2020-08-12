39ad79adff8815bda0b65b07d98d697f33bf3f15
nachman alush <newnachman@gmail.com>
2020-08-12 11:34:49 +0300

    uploaded core files too

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,9 +2,55 @@ import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  template: `
+  <nav 
+  *ngIf='cond'
+  class="navbar navbar-expand navbar-light bg=light">
+  <p  class="navbar-brand">{{title}}</p>
+  
+  <ul class="nav nav-pills">
+      <li style="display:inline; border: 2px solid red; margin: 5px; padding: 5px;" class="nav-link" [routerLink]="['/welcome']">Home</li>
+      <li style="display:inline; border: 2px solid red; margin: 5px; padding: 5px;" class="nav-link" [routerLink]="['/instructors']">Instructors List</li>
+  </ul>
+</nav>
+<div class='container'>
+  <router-outlet></router-outlet>
+</div>
+
+  
+  
+   
+  `
 })
 export class AppComponent {
-  title = 'instructor-angular';
+  title = 'instructor-angular **title** from app.component';
+  cond: boolean = true;
+   tempList: any[] = [{
+    "instId": 1,
+    "instName": "meir chaviv",
+    "insrField": "sports"
+},
+{
+    "instId": 2,
+    "instName": "shalom dvir",
+    "insrField": "arts"
+},
+{
+    "instId": 3,
+    "instName": "shalom dvir2",
+    "insrField": "arts"
 }
+]; 
+}
+// <router-outlet></router-outlet>
+// 
+// <div>
+// 	    <h1>{{title}}</h1>
+// 	     <inst-list></inst-list>
+// 	  </div>
+
+//<div class="kdkd" 
+// *ngFor =  'let inst of tempList'
+// >
+// <div> {{inst.instId}} </div>
+// </div>  
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,27 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { RouterModule } from '@angular/router';
+import { HttpClientModule } from '@angular/common/http';
+
 
 import { AppComponent } from './app.component';
+import { InstructorsModule } from './instructors/instructors.module';
+import { WelcomeComponent } from './home/welcome.component';
 
 @NgModule({
-  declarations: [
-    AppComponent
+  declarations: [ 
+    AppComponent,
+    WelcomeComponent
   ],
   imports: [
-    BrowserModule
+    HttpClientModule,
+    BrowserModule,
+    InstructorsModule,
+    RouterModule.forRoot([
+      { path: 'welcome', component: WelcomeComponent },
+      { path: '', redirectTo: 'welcome', pathMatch: 'full' },
+      { path: '**', redirectTo: 'welcome', pathMatch: 'full' }
+    ])
   ],
   providers: [],
   bootstrap: [AppComponent]
